e77a8bc77204553f453764493318fb1ad7332ff1
Ebony <ebony.williams@gmail.com>
2021-03-30 16:48:17 -0400

    adds svgs, styles accordion

--- faq-accordion-card-main/script/app.js
+++ faq-accordion-card-main/script/app.js
@@ -1,14 +1,24 @@
 window.onload = function () {
     let accordion = document.getElementsByClassName("accordion");
+    let panel = document.getElementsByClassName('panel');
+   
     for (i = 0; i < accordion.length; i++) {
-        accordion[i].addEventListener("click", function () {
-            this.classList.toggle("active");
-            let panel = this.nextElementSibling;
-            if (panel.style.display === "block") {
-                panel.style.display = "none";
-            } else {
-                panel.style.display = "block";
+        accordion[i].onclick = function () {
+            let setClasses = !this.classList.contains('active');
+            setClass(accordion, 'active', 'remove');
+            setClass(panel, 'show', 'remove');
+
+            if (setClasses) {
+                this.classList.toggle("active");
+                this.nextElementSibling.classList.toggle("show"); 
             }
-        });
+        }
     }
+
+    function setClass(element, className, functionName) {
+        for (let i = 0; i < element.length; i++) {
+            element[i].classList[functionName](className);
+        }
+    }
+
 }
